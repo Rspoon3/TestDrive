4f1d3f7c3caf553191cbd663d29a59b32c70316e
Ricky Witherspoon <42879920+Rspoon3@users.noreply.github.com>
2025-06-13 15:32:40 -0400

    setup

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        NavigationStack {
+            ScrollView {
+                ForEach(0..<100, id: \.self) { _ in
+                    Color.red
+                        .frame(height: 140)
+                }
+            }
+            .safeAreaInset(edge: .bottom, alignment: .trailing) {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "star.circle.fill")
+                        .resizable()
+                        .frame(width: 44, height: 44)
+                        .padding([.trailing, .bottom])
+                        .foregroundStyle(.white, .blue)
+                }
+            }
+        }
     }
 }
 
