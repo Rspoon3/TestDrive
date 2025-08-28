e31eb116cbeb1125764ce7c25fd4ceee6e9711b2
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2025-08-28 09:39:55 -0400

    opens for deeplinks

--- iOS/ContentView.swift
+++ iOS/ContentView.swift
@@ -8,11 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Binding var deepLinkURL: URL?
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 20) {
+            Text("TestDrive Deep Link Handler")
+                .font(.title)
+                .padding()
+            
+            if let url = deepLinkURL {
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Received Deep Link:")
+                        .font(.headline)
+                    
+                    Text(url.absoluteString)
+                        .font(.system(.body, design: .monospaced))
+                        .padding()
+                        .background(Color.gray.opacity(0.1))
+                        .cornerRadius(8)
+                    
+                    if let scheme = url.scheme {
+                        Text("Scheme: \(scheme)")
+                            .foregroundColor(.blue)
+                    }
+                    
+                    if let host = url.host {
+                        Text("Host: \(host)")
+                            .foregroundColor(.blue)
+                    }
+                    
+                    if !url.pathComponents.filter({ $0 != "/" }).isEmpty {
+                        Text("Path: \(url.path)")
+                            .foregroundColor(.blue)
+                    }
+                    
+                    if let query = url.query {
+                        Text("Query: \(query)")
+                            .foregroundColor(.blue)
+                    }
+                }
+                .padding()
+                .background(Color.blue.opacity(0.05))
+                .cornerRadius(12)
+            } else {
+                Text("No deep link received yet")
+                    .foregroundColor(.gray)
+                
+                Text("Try opening a testdrive:// URL")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+            }
+        }
+        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(deepLinkURL: .constant(nil))
 }
--- iOS/TestDriveApp.swift
+++ iOS/TestDriveApp.swift
@@ -9,9 +9,15 @@ import SwiftUI
 
 @main
 struct TestDriveApp: App {
+    @State private var deepLinkURL: URL?
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(deepLinkURL: $deepLinkURL)
+                .onOpenURL { url in
+                    print("Received deep link: \(url)")
+                    deepLinkURL = url
+                }
         }
     }
 }
